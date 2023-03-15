5a778755eb879cd1e269154fca09d54d08116180
son189 <son.august.7334@gmail.com>
2023-03-15 15:45:01 +0300

    done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -9,7 +9,14 @@ class CheesesController < ApplicationController
   # GET /cheeses/:id
   def show
     cheese = Cheese.find_by(id: params[:id])
-    render json: cheese
+    
+    #render json: cheese, only: [:id , :name , :price, :is_best_seller]
+     # render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+     if cheese
+      render json: cheese, except: [:created_at, :updated_at], methods: [:summary]
+    else
+      render json: { error: 'Cheese not found' } status: :not_found
+    end
   end
 
 end
